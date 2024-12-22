39a34b097c38795a697a6b9e01b060140e2976f6
Steven Gibson <devgibsonsp@gmail.com>
2024-12-22 14:19:07 -0500

    stubbing out project: structure, dependencies, tests, etc

---
+++ Api/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace LibraryApp.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+
+        private readonly ILogger<TestController> _logger;
+
+        public BooksController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetBooks")]
+        public bool Get()
+        {
+            return true;
+        }
+    }
+}
---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace LibraryApp.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public UserController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetUsers")]
+        public bool Get()
+        {
+            return true;
+        }
+    }
+}
