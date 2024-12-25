168358d983bbc9088b702302218dabdb494ad2ac
Steven Gibson <devgibsonsp@gmail.com>
2024-12-25 15:09:26 -0500

    added component library and fleshed out site more

--- Site/src/router/Routes.tsx
+++ Site/src/router/Routes.tsx
@@ -1,34 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home/Home";
-//import BookDetails from "../pages/BookDetails/BookDetails";
-//import ManageBooks from "../pages/ManageBooks/ManageBooks";
-//import Login from "../pages/Login/Login";
-//import Signup from "../pages/Signup/Signup";
-//import NotFound from "../pages/NotFound";
-/*
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home/Home"; // Replace with the actual path to your Home component
+import Dashboard from "../pages/Dashboard/Dashboard"; // Replace with the actual path to your Dashboard component v
+
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/book/:id" element={<BookDetails />} />
-        <Route path="/manage-books" element={<ManageBooks />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
 };
-*/
 
-const AppRoutes = () => {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    );
-  };
 export default AppRoutes;
